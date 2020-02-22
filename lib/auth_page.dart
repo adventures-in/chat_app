4cb4c830f005c1ad2ec884bfa2bbeddadeba9228
Nick M <nick.meinhold@gmail.com>
2020-02-22 13:20:15 +1100

    Merge latest master

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class AuthPage extends StatelessWidget {
   @override
@@ -12,13 +13,17 @@ class AuthPage extends StatelessWidget {
         children: <Widget>[
           GoogleSignInButton(
             onPressed: () async {
-              _signin(context).listen((event) {
+              _googleSignin(context).listen((event) {
                 print(event);
               });
             },
           ),
           FacebookSignInButton(
-            onPressed: () {},
+            onPressed: () async {
+              _facebookSignin(context).listen((event) {
+                print(event);
+              });
+            }
           ),
         ],
       ),
@@ -26,7 +31,7 @@ class AuthPage extends StatelessWidget {
   }
 }
 
-Stream<int> _signin(BuildContext context) async* {
+Stream<int> _googleSignin(BuildContext context) async* {
   try {
     final _fireAuth = FirebaseAuth.instance;
     final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>['email']);
@@ -62,11 +67,53 @@ Stream<int> _signin(BuildContext context) async* {
     // errors with code kSignInCanceledError are swallowed by the
     // GoogleSignIn.signIn() method so we can assume anything caught here
     // is unexpected and for display
-    _showDialog(context);
+    _showDialog(context, error.toString());
   }
 }
 
-void _showDialog(BuildContext context) {
+Stream<int> _facebookSignin(BuildContext context) async* {
+  try {
+    final _fireAuth = FirebaseAuth.instance;
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logIn(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        /// the auth info will be picked up by the listener on [onAuthStateChanged]
+        /// and emitted by [streamOfStateChanges]
+        
+        // signal to change UI
+        yield 2;
+
+        final credential = FacebookAuthProvider.getCredential(accessToken: result.accessToken.token);
+        await _fireAuth.signInWithCredential(credential);
+
+        // we are signed in so reset the UI
+        yield 0;
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        // _showCancelledMessage();
+        yield 0;
+        break;
+      case FacebookLoginStatus.error:
+        // _showErrorOnUI(result.errorMessage);
+        yield 0;
+        throw result.errorMessage;
+        break;
+    }
+
+  } catch (error, trace) {
+    // reset the UI and display an alert
+
+    yield 0;
+    // errors with code kSignInCanceledError are swallowed by the
+    // GoogleSignIn.signIn() method so we can assume anything caught here
+    // is unexpected and for display
+    _showDialog(context, error.toString());
+  }
+}
+
+void _showDialog(BuildContext context, String errorMessage) {
   // flutter defined function
   showDialog(
     context: context,
@@ -74,7 +121,7 @@ void _showDialog(BuildContext context) {
       // return object of type Dialog
       return AlertDialog(
         title: new Text("Alert Dialog title"),
-        content: new Text("Alert Dialog body"),
+        content: new Text(errorMessage),
         actions: <Widget>[
           // usually buttons at the bottom of the dialog
           new FlatButton(
