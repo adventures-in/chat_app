f1d64a1dcaa8234e14875d9d245028cc07249033
Nick Meinhold <nick.meinhold@gmail.com>
2020-03-12 21:11:20 +1100

    Setup and update android project for notifications

---
+++ android/app/src/main/kotlin/network/adventuresin/meetup_chatapp/Application.kt
@@ -0,0 +1,19 @@
+package network.adventuresin.meetup_chatapp;
+
+import io.flutter.app.FlutterApplication
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+        GeneratedPluginRegistrant.registerWith((registry as FlutterEngine))
+    }
+}
