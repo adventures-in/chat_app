a2de7d72e58ad55df7ac444154ec541ac053dd05
Nick Meinhold <nick.meinhold@gmail.com>
2020-04-16 01:38:55 +1000

    Fix bug: remove duplicate code from AppDelegate
    - AppDelegate was requesting authorization and registering
       but the package can do this (and can be called at any time)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,23 +9,10 @@ import Flutter
   ) -> Bool {
 
     if #available(iOS 10.0, *) {
-      // For iOS 10 display notification (sent via APNS)
-      UNUserNotificationCenter.current().delegate = self
-
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: {_, _ in })
-    } else {
-      let settings: UIUserNotificationSettings =
-      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
-
-    application.registerForRemoteNotifications()
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
