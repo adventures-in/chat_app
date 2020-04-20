5b6d670589a45cbb0a7e0634d870eb89802f2a3e
Nick Meinhold <nick.meinhold@gmail.com>
2020-04-20 14:57:38 +1000

    Build android on macOS during CICD workflow
    - add a shell script for downloading all credential files
    - update ios workflow file to build android

---
+++ get-credentials.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ 
+echo "Retrieving credential files..."
+
+gsutil cp gs://adventures-in-credentials/GoogleService-Info.plist ./ios/Runner/GoogleService-Info.plist
+gsutil cp gs://adventures-in-credentials/gc_keys.json ./ios/gc_keys.json
+gsutil cp gs://adventures-in-credentials/google-services.json ./android/app/google-services.json
+gsutil cp gs://adventures-in-credentials/keystore.jks ./android/app/keystore.jks
+gsutil cp gs://adventures-in-credentials/keystore.config ./android/app/keystore.config
