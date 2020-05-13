175b9cbb9e920e81a476ca63cff7f2c062726545
David Micallef <david.j.micallef@gmail.com>
2020-05-13 15:40:42 +1000

    Feature/issue 78 add timestamp to message (#116)
    * Set theme jekyll-theme-tactile
    
    * Added timestamp to the row
    
    Co-authored-by: Adventures In <59868030+adventuresin@users.noreply.github.com>
    Co-authored-by: David Micallef <david.micallef@travelanalytics.com.au>

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -2,28 +2,41 @@ import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
   final String text;
+  final DateTime dateTime;
 
-  const ChatMessage({Key key, this.text}) : super(key: key);
+  const ChatMessage({Key key, this.text, this.dateTime}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var formattedDate =
+        '${dateTime.year}-${dateTime.month}-${dateTime.day} ${dateTime.hour}:${dateTime.minute}';
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(text,
-                style: theme.textTheme.bodyText1.merge(TextStyle(
-                    fontSize: 20,
-                    background: Paint()
-                      ..strokeWidth = 30.0
-                      ..color = theme.backgroundColor
-                      ..style = PaintingStyle.stroke
-                      ..strokeJoin = StrokeJoin.round)))),
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(text,
+                  style: theme.textTheme.bodyText1.merge(TextStyle(
+                      fontSize: 20,
+                      background: Paint()
+                        ..strokeWidth = 30.0
+                        ..color = theme.backgroundColor
+                        ..style = PaintingStyle.stroke
+                        ..strokeJoin = StrokeJoin.round))),
+            ),
+            Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(formattedDate,
+                    style: theme.textTheme.bodyText1
+                        .merge(TextStyle(fontSize: 12)))),
+          ],
+        ),
       ],
     );
   }
