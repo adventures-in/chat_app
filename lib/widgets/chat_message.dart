0c0fb5cb6f48b4ef24cbb756d6ae10cf30bfb48e
David Micallef <david.j.micallef@gmail.com>
2020-05-23 12:52:47 +1000

    Fixup/message overflow (#132)
    * Query conversations directly rather than using conversation-items
    Fixes #124
    
    * Remove problematic ios build changes
    
    * Following example #25
    
    * padding helps for minute
    
    Co-authored-by: Nick Meinhold <nick.meinhold@gmail.com>
    Co-authored-by: David Micallef <david.micallef@travelanalytics.com.au>

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -11,7 +11,7 @@ class ChatMessage extends StatelessWidget {
     var theme = Theme.of(context);
     var formattedDate = (dateTime == null)
         ? ''
-        : '${dateTime.year}-${dateTime.month}-${dateTime.day} ${dateTime.hour}:${dateTime.minute}';
+        : '${dateTime.year}-${dateTime.month}-${dateTime.day} ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, "0")}';
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -20,16 +20,18 @@ class ChatMessage extends StatelessWidget {
       children: [
         Row(
           children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(text,
-                  style: theme.textTheme.bodyText1.merge(TextStyle(
-                      fontSize: 20,
-                      background: Paint()
-                        ..strokeWidth = 30.0
-                        ..color = theme.backgroundColor
-                        ..style = PaintingStyle.stroke
-                        ..strokeJoin = StrokeJoin.round))),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(text,
+                    style: theme.textTheme.bodyText1.merge(TextStyle(
+                        fontSize: 20,
+                        background: Paint()
+                          ..strokeWidth = 30.0
+                          ..color = theme.backgroundColor
+                          ..style = PaintingStyle.stroke
+                          ..strokeJoin = StrokeJoin.round))),
+              ),
             ),
             Padding(
                 padding: EdgeInsets.all(16.0),
