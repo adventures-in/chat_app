4e79ba425bad9be38f346c83aa4a0fd93fae91be
David Micallef <david.micallef@travelanalytics.com.au>
2020-06-14 07:35:24 +1000

    add test for populateWith using simular methods as add

--- test/unit/conversations_page_test.dart
+++ test/unit/conversations_page_test.dart
@@ -5,7 +5,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('group', () {
-    test('test ViewModel', () {
+    test('test ConversationViewModel add', () {
       var inject_list = <ConversationItem>[];
       var vm = ConversationsViewModel(inject_list);
       var instance = ConversationItem(
@@ -15,6 +15,20 @@ void main() {
           photoURLs: ['https://url1', 'https://url2']);
       vm.add(item: instance);
       expect(inject_list, [instance]);
+    });
+    test('test ConversationViewModel populateWith', () {
+      var inject_list = <ConversationItem>[];
+      var vm = ConversationsViewModel(inject_list); // start with empty list
+      var instance = ConversationItem(
+          conversationId: 'abc123',
+          uids: ['123', '456'],
+          displayNames: ['Leon', 'Noel'],
+          photoURLs: ['https://url1', 'https://url2']);
+      var alternate_list = <ConversationItem>[];
+      alternate_list.add(instance);
+
+      vm.populateWith(alternate_list);
+      expect(inject_list, alternate_list);
     });
   });
 }
