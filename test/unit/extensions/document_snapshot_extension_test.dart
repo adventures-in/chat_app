1e1de8bb843c5d4564422aa80587f57e637d6f2f
David Micallef <david.micallef@travelanalytics.com.au>
2020-06-23 06:55:43 +1000

    PR review comments

--- test/unit/extensions/document_snapshot_extension_test.dart
+++ test/unit/extensions/document_snapshot_extension_test.dart
@@ -5,8 +5,10 @@ import 'package:adventures_in_chat_app/extensions/extensions.dart';
 import '../../mocks/document_snapshot_mocks.dart';
 
 void main() {
-  group('toMessage()', () {
-    test('will return valid DateTime when valid Timestamp in database', () {
+  group('DocumentSnapshot', () {
+    test(
+        '.toMessage() will return valid DateTime when valid Timestamp in database',
+        () {
       var fake = FakeDocumentSnapshot.asMessage('123455679', 'Test Message',
           Timestamp.fromDate(DateTime.parse('1969-07-20 20:18:04')));
       var message = fake.toMessage();
@@ -14,7 +16,8 @@ void main() {
       expect(message.timestamp, DateTime.parse('1969-07-20 20:18:04'));
     });
 
-    test('will return current DateTime when in null Timestamp', () {
+    test('.toMessage() will return current DateTime when in null Timestamp',
+        () {
       var fake =
           FakeDocumentSnapshot.asMessage('123455679', 'Test Message', null);
       var before = DateTime.now();
@@ -26,10 +29,10 @@ void main() {
           message.timestamp.isAfter(before) &&
               message.timestamp.isBefore(after));
     });
-  });
 
-  group('toUserItem()', () {
-    test('will have empty displayName and photoURL if not populated', () {
+    test(
+        '.toUserItem() will have empty displayName and photoURL if not populated',
+        () {
       var fake = FakeDocumentSnapshot.nullData();
       var userItem = fake.toUserItem();
 
@@ -37,7 +40,7 @@ void main() {
       expect(userItem.photoURL, null);
     });
 
-    test('will have displayName and photoURL', () {
+    test('.toUserItem() will have displayName and photoURL', () {
       var fake = FakeDocumentSnapshot.asUserItem(
           'leon', 'https://example.com/leon.png');
       var userItem = fake.toUserItem();
