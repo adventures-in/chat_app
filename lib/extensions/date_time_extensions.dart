9ab168b6e1f9cc4b36ad7f3208ad0daf5940c2a5
David Micallef <david.micallef@travelanalytics.com.au>
2020-07-07 19:51:40 +1000

    just added the one test for printDate

--- lib/extensions/date_time_extensions.dart
+++ lib/extensions/date_time_extensions.dart
@@ -1,5 +1,8 @@
 extension DateOnlyCompare on DateTime {
   bool isSameDate(DateTime other) {
+    if (other == null) {
+      return false;
+    }
     return year == other.year && month == other.month && day == other.day;
   }
 
--- test/unit/extensions/date_time_extensions_test.dart
+++ test/unit/extensions/date_time_extensions_test.dart
@@ -2,40 +2,47 @@ import 'package:test/test.dart';
 import 'package:adventures_in_chat_app/extensions/extensions.dart';
 
 void main() {
-  group('DateTime', () {
-    test('.isSameDate() returns True when earlier in same day', () {
+  group('DateTime.isSameDate()', () {
+    test('returns True when earlier in same day', () {
       var t0 = DateTime.parse('1969-07-20 20:18:04');
       var t1 = DateTime.parse('1969-07-20 08:28:34');
 
       expect(true, t0.isSameDate(t1));
     });
 
-    test('.isSameDate() returns True when later in same day', () {
+    test('returns True when later in same day', () {
       var t0 = DateTime.parse('1969-07-20 20:18:04');
       var t1 = DateTime.parse('1969-07-20 23:59:34');
 
       expect(true, t0.isSameDate(t1));
     });
 
-    test('.isSameDate() returns False when compared to null', () {
+    test('returns False when compared to null', () {
       var t0 = DateTime.parse('1969-07-20 20:18:04');
       DateTime t1;
 
       expect(false, t0.isSameDate(t1));
-    }, skip: true);
+    }, skip: false);
 
-    test('.isSameDate() returns False when compared to day before', () {
+    test('returns False when compared to day before', () {
       var t0 = DateTime.parse('1969-07-20 20:18:04');
       var t1 = DateTime.parse('1969-07-19 23:59:34');
 
       expect(false, t0.isSameDate(t1));
     });
 
-    test('.isSameDate() returns False when compared to day after', () {
+    test('returns False when compared to day after', () {
       var t0 = DateTime.parse('1969-07-20 20:18:04');
       var t1 = DateTime.parse('1969-07-21 23:59:34');
 
       expect(false, t0.isSameDate(t1));
+    });
+  });
+
+  group('DateTime.printDate()', () {
+    test('returns the date in format day/month/year', () {
+      var t0 = DateTime.parse('1969-07-20 20:18:04');
+      expect(t0.printDate(), '20/7/1969');
     });
   });
 }
