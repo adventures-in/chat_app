3b0ae303f03e2203da7740c99a6933ec3be83311
Nick Meinhold <nick.meinhold@gmail.com>
2020-07-11 14:53:02 +1000

    stream already been listened to when user clicked on their second con… (#211)
    * stream already been listened to when user clicked on their second conversation
    Fixes #205
    
    - changed to a stream transformer
    
    * Added a fix to ensure a date is added on first day

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -12,37 +12,35 @@ import 'package:adventures_in_chat_app/extensions/extensions.dart';
 class DatabaseService {
   String currentUserId;
   final Database _database;
-  final _controllers = <String, StreamController<List<MessagesListItem>>>{};
 
   DatabaseService({Database database})
       : _database = database ?? FirestoreDatabase();
 
   Stream<List<MessagesListItem>> getMessagesStream(String conversationId) {
-    // fresh controller per unique conversation.
-    StreamController<List<MessagesListItem>> _controller;
-    if (_controllers.containsKey(conversationId)) {
-      _controller = _controllers[conversationId];
-    } else {
-      _controller = StreamController<List<MessagesListItem>>();
-    }
+    return _database
+        .getMessagesStream(conversationId)
+        .transform<List<MessagesListItem>>(
+            StreamTransformer.fromHandlers(handleData: (messagesList, sink) {
+      // if the list is empty just emit an empty list and return
+      if (messagesList.isEmpty) {
+        sink.add(<MessagesListItem>[]);
+        return;
+      }
 
-    _database.getMessagesStream(conversationId).listen((messagesList) {
-      var latest = DateTime.fromMicrosecondsSinceEpoch(0); // init to minimum
-      final newList = <MessagesListItem>[];
+      var latest = messagesList.first.timestamp;
+      final itemsList = <MessagesListItem>[SectionDate(latest)];
 
       for (final message in messagesList) {
         // if message is first in a new day - insert SectionDate
         if (!message.timestamp.isSameDate(latest)) {
           latest = message.timestamp;
-          newList.add(SectionDate(latest));
+          itemsList.add(SectionDate(latest));
         }
-        newList.add(message);
+        itemsList.add(message);
       }
 
-      _controller.add(newList);
-    });
-
-    return _controller.stream;
+      sink.add(itemsList);
+    }));
   }
 
   Future<String> sendMessage({
